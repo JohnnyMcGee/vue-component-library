7ec4cbcfc9ab01445fb602cded2d26eb62410dd9
Johnny McGee <johnnymcgee.dev@gmail.com>
2022-12-07 10:37:24 -0600

    Initialize vue library

---
+++ src/components/Button.vue
@@ -0,0 +1,29 @@
+<script lang="ts" setup>
+defineProps({
+    small: {
+        type: Boolean,
+        default: false
+    },
+    color: {
+        type: String,
+        default: "#f46663"
+    }
+});
+</script>
+
+<template>
+    <button :class="{btn: true, 'btn--small': small}" :style="{backgroundColor: color}">
+        <slot></slot>
+    </button>
+</template>
+
+<style scoped>
+.btn {
+    padding: 16px 32px;
+    border-radius: 48px;
+}
+
+.btn--small {
+    padding: 12px 24px;
+}
+</style>
